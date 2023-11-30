321a07599f569ece5955413573ab35efd9c30965
pseudonymtim <tim.earl@rvu.co.uk>
2023-11-30 13:11:34 +0000

    2022 day 1 solution and setup for 2023

---
+++ src/y2022/d01/solution.clj
@@ -0,0 +1,46 @@
+(ns y2022.d01.solution
+  (:require [clojure.java.io :as io]))
+
+(def file "src/y2022/d01/input.txt")
+
+(defn read-data
+  [file]
+  (with-open [rdr (io/reader file)]
+    (into [] (line-seq rdr))))
+
+(defn parse
+  [calories-str]
+  (if (empty? calories-str)
+    nil
+    (parse-long calories-str)))
+
+(def totals
+  (->> (read-data file)
+       (map parse)
+       (partition-by nil?)
+       (remove #(nil? (first %)))
+       (map #(apply + %))))
+
+(def named-totals
+  (seq (zipmap (range 1 (inc (count totals)))
+               totals)))
+
+(defn desc-comp
+  [[n1 c1] [n2 c2]]
+  (compare c2 c1))
+
+(def desc-sorted-named-totals
+  (sort desc-comp named-totals))
+
+;; How many total Calories is that Elf carrying?
+(def most-calories-being-carried
+  (->> named-totals
+       (sort desc-comp)
+       first
+       second))
+
+;; How many Calories are those Elves carrying in total?
+(def calories-carried-by-to-three-carriers)
+(->> (take 3 desc-sorted-named-totals)
+     (map second)
+     (apply +))
--- src/y2023/d01/solution.clj
+++ src/y2023/d01/solution.clj
@@ -1,1 +1,9 @@
-(ns y2023.d01.solution)
+(ns y2023.d01.solution
+  (:require [clojure.java.io :as io]))
+
+(def file "src/y2023/d01/input.txt")
+
+(defn read-data
+  [file]
+  (with-open [rdr (io/reader file)]
+    (into [] (line-seq rdr))))
