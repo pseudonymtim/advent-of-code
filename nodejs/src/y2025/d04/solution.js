849201719b23312e4d7d9264d0b35307bbc81cbf
Tim Earl <timcearl@gmail.com>
2026-01-31 19:50:50 +0000

    Finished 2025 day 4

--- nodejs/src/y2025/d04/solution.js
+++ nodejs/src/y2025/d04/solution.js
@@ -1,8 +1,7 @@
 const fs = require('node:fs');
-const ld = require('lodash');
 
-const input = '/Users/tim/Documents/dev/advent-of-code/nodejs/src/y2025/d03/input.txt'
-const sampleInput = '/Users/tim/Documents/dev/advent-of-code/nodejs/src/y2025/d03/sample-input.txt'
+const input = '/Users/tim/Documents/dev/advent-of-code/nodejs/src/y2025/d04/input.txt'
+const sampleInput = '/Users/tim/Documents/dev/advent-of-code/nodejs/src/y2025/d04/sample-input.txt'
 
 var data;
 
@@ -13,84 +12,176 @@ try {
   console.error('Error reading the file: ' + err);
 }
 
+const paperRolls = data.split("\n").map((array) => array.split(""));
+
+const spaceWithRoll = '@';
+const emptySpace = '.';
+
 // Part 1
 
-function toNumber(s) {
-  return Number(s);
+function above(i) {
+  return i - 1;
 }
 
-function toNumbers(numbers) {
-  return numbers.map((s) => toNumber(s))
+function below(i) {
+  return i + 1;
 }
 
-function biggestTwoDigitNumber(numbers) {
-  // Remove last element
-  const lastNumber = numbers.pop();
-  const biggest = Math.max(...numbers);
-
-  const indexOfBiggest = numbers.indexOf(biggest);
-
-  const slicedNumbers = numbers.slice(indexOfBiggest+1, numbers.length);
-  slicedNumbers.push(lastNumber);
-  const nextBiggest = Math.max(...slicedNumbers);
-
-  return Number(biggest + "" + nextBiggest);
+function left(i) {
+  return i - 1;
 }
 
-console.log(
-  data.split("\n")
-      .map((s) => s.split(""))
-      .map(toNumbers)
-      .map((numbers) => biggestTwoDigitNumber(numbers))
-      .reduce((total, num) => total + num)
-  );
+function right(i) {
+  return i + 1;
+}
+
+function aboveValid(i) {
+  return above(i) >= 0;
+}
+
+function belowValid(i, length) {
+  return below(i) < length;
+}
+
+function leftValid(i) {
+  return left(i) >= 0;
+}
+
+function rightValid(i, length) {
+  return right(i) < length;
+}
+
+function aboveLeft(v, h, paperRolls) {
+    return aboveValid(v) && leftValid(h)
+      ? paperRolls[above(v)][left(h)]
+      : emptySpace;
+}
+
+function aboveMiddle(v, h, paperRolls) {
+    return aboveValid(v)
+      ? paperRolls[above(v)][h]
+      : emptySpace;
+}
+
+function aboveRight(v, h, paperRolls) {
+    return aboveValid(v) && rightValid(h,paperRolls[above(v)].length)
+      ? paperRolls[above(v)][right(h)]
+      : emptySpace;
+}
+
+function leftMiddle(v, h, paperRolls) {
+    return leftValid(h)
+      ? paperRolls[v][left(h)]
+      : emptySpace;
+}
+
+function rightMiddle(v, h, paperRolls) {
+    return rightValid(h,paperRolls[v].length)
+      ? paperRolls[v][right(h)]
+      : emptySpace;
+}
+
+function belowLeft(v, h, paperRolls) {
+    return belowValid(v,paperRolls.length) && leftValid(h)
+      ? paperRolls[below(v)][left(h)]
+      : emptySpace;
+}
+
+function belowMiddle(v, h, paperRolls) {
+    return belowValid(v,paperRolls.length)
+      ? paperRolls[below(v)][h]
+      : emptySpace;
+}
+
+function belowRight(v, h, paperRolls) {
+    return belowValid(v,paperRolls.length) && rightValid(h,paperRolls[below(v)].length)
+      ? paperRolls[below(v)][right(h)]
+      : emptySpace;
+}
+
+
+
+let accessiblePaperRolls = 0;
+
+for (let v = 0; v < paperRolls.length; v++) {
+  for (let h = 0; h < paperRolls[v].length; h++) {
+    let surroundings = [ aboveLeft(v,h,paperRolls), aboveMiddle(v,h,paperRolls), aboveRight(v,h,paperRolls),
+                        leftMiddle(v,h,paperRolls),                              rightMiddle(v,h,paperRolls),
+                         belowLeft(v,h,paperRolls), belowMiddle(v,h,paperRolls), belowRight(v,h,paperRolls)];
+    let rolls = surroundings.filter((x) => x == spaceWithRoll).length
+
+    let currentSpot = paperRolls[v][h];
+
+    if (rolls < 4 && currentSpot == spaceWithRoll) {
+      accessiblePaperRolls++;
+    }
+
+//    console.log("accessible=" + accessiblePaperRolls
+//                + " v=" + v
+//                + " h=" + h
+//                + " rolls=" + rolls
+//                + " around=" + surroundings);
+
+  }
+}
+
+console.log("Part 1: " + accessiblePaperRolls);
+
 
 // ------------------------------
 
 // Part 2
 
-function biggestDigit(numbers) {
-  return Math.max(...numbers);
+
+function rollsThatCanBeRemoved(paperRolls) {
+
+  let rollsToRemove = [];
+
+  for (let v = 0; v < paperRolls.length; v++) {
+    for (let h = 0; h < paperRolls[v].length; h++) {
+      let surroundings = [ aboveLeft(v,h,paperRolls), aboveMiddle(v,h,paperRolls), aboveRight(v,h,paperRolls),
+                          leftMiddle(v,h,paperRolls),                              rightMiddle(v,h,paperRolls),
+                           belowLeft(v,h,paperRolls), belowMiddle(v,h,paperRolls), belowRight(v,h,paperRolls)];
+      let rolls = surroundings.filter((x) => x == spaceWithRoll).length
+
+      let currentSpot = paperRolls[v][h];
+
+      if (rolls < 4 && currentSpot == spaceWithRoll) {
+        rollsToRemove.push([v,h]);
+      }
+
+  //    console.log("accessible=" + rollsToRemove.length
+  //                + " v=" + v
+  //                + " h=" + h
+  //                + " rolls=" + rolls
+  //                + " around=" + surroundings);
+
+    }
+  }
+
+  return rollsToRemove;
 }
 
-function biggetVoltage(numbers, digits) {
-  if (numbers.length < digits) {
-    throw new Error("Not enough numbers");
+function removeRolls(paperRolls, rollsToRemoveThisRound) {
+  for(const [v,h] of rollsToRemoveThisRound) {
+    paperRolls[v][h] = emptySpace;
   }
-
-  if (numbers.length == digits) {
-    return Number(...numbers);
-  }
-
-  const biggestNumbers = [];
-
-  let startIndex = 0;
-  let endIndex = numbers.length - digits + 1;
-
-  while (biggestNumbers.length < digits) {
-
-    const numbersToSearch = numbers.slice(startIndex, endIndex);
-    const biggest = biggestDigit(numbersToSearch);
-    biggestNumbers.push(biggest);
-
-    // Update slice
-    const indexOfBiggest = numbersToSearch.indexOf(biggest);
-
-    startIndex = startIndex + indexOfBiggest + 1;
-    endIndex++;
-  }
-
-  return Number(biggestNumbers.join(""));
+  return paperRolls;
 }
 
-console.log(
-  data.split("\n")
-      .map((s) => s.split(""))
-      .map(toNumbers)
-      .map((numbers) => biggetVoltage(numbers, 12))
-      .reduce((total, num) => total + num)
-  );
+function howManyRollsCouldBeRemoved(paperRolls) {
 
-let nnn;
+  let allRollsRemoved = [];
 
-console.log(nnnn);
+  let rollsToRemoveThisRound = rollsThatCanBeRemoved(paperRolls);
+
+  while (rollsToRemoveThisRound.length != 0) {
+    allRollsRemoved = allRollsRemoved.concat(rollsToRemoveThisRound)
+    let newPaperRolls = removeRolls(paperRolls, rollsToRemoveThisRound);
+    rollsToRemoveThisRound = rollsThatCanBeRemoved(newPaperRolls);
+  }
+
+  return allRollsRemoved.length;
+}
+
+console.log("Part 2: " + howManyRollsCouldBeRemoved(paperRolls));
